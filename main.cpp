2eb39e372486c97b9a365de623801f7346a78475
Matthew Hebert <42185853+hebertmatthew98@users.noreply.github.com>
2019-01-02 14:38:15 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include "wordFinder.h"
+
+using namespace std;
+
+int main()
+{
+	string subSeq = "abppplee";
+	//cout << "Please enter a subsequence you want me to find the longest word of." << endl;
+	//cin >> subSeq;
+
+	wordFinder Scan(subSeq);
+
+	return 0;
+}
